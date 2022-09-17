21de83a70c9d3059877e314546a7be9e2703f9c3
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-09-17 21:32:55 +0430

    implement search by character name feature in index.html

--- js-rick-and-morty-api/assets/js/alpine.js
+++ js-rick-and-morty-api/assets/js/alpine.js
@@ -38,3 +38,37 @@ function getCharacters() {
 		}
 	}
 }
+
+function filterCharacters() {
+	return {
+		url: null,
+		filter: '',
+		characters: null,
+		fetchStatus: null,
+		init() {
+			this.$watch('filter', () => {
+				this.filterData();
+			});
+		},
+		filterData() {
+			this.fetchStatus = 'loading',
+				fetch(`https://rickandmortyapi.com/api/character/?name=${this.filter}`)
+					.then(res => {
+						if (!res.ok) {
+							this.fetchStatus = 'error';
+						}
+						return res.json();
+					})
+					.then(data => {
+						this.fetchStatus = "success";
+						this.characters = data.results;
+					})
+					.catch(error => {
+						this.fetchStatus = 'error';
+					});
+		},
+		link(id) {
+			this.url = 'character.html?char=' + id;
+		}
+	}
+}
