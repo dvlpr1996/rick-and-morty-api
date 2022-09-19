869feec7c0befa33d32d791241455bd8db4a1589
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-09-20 02:30:51 +0430

    implement travel feature in portal page

--- php-rick-and-morty-api/app/controllers/PortalController.php
+++ php-rick-and-morty-api/app/controllers/PortalController.php
@@ -2,7 +2,9 @@
 
 namespace app\controllers;
 
+use app\utilities\Config;
 use Buki\Router\Http\Controller;
+use app\core\adapter\GuzzleAdapter;
 
 class PortalController extends Controller
 {
@@ -13,6 +15,7 @@ class PortalController extends Controller
 
 	public function show()
 	{
-		view('portal');
+		$location = GuzzleAdapter::response(Config::get('api.travel'));
+		view('portal', compact('location'));
 	}
 }
--- php-rick-and-morty-api/resource/views/portal.blade.php
+++ php-rick-and-morty-api/resource/views/portal.blade.php
@@ -5,13 +5,20 @@
 @section('content')
 		<main class="mt-8 mb-16 flex flex-col items-center justify-center space-y-5">
 				<div class="space-y-5">
+						<h1 class="text-center">travel between universes</h1>
+
 						<img src="{{ asset('img/portal.png') }}" alt="portal" class="mx-auto block" id="portal">
-					
-					
+						@if (isset($location->name))
+								<p class="text-center text-xl font-semibold">you are in
+										<strong class="text-white italic">{{ $location->name }}</strong>
+										and type of this location is data.type in the
+										<strong class="text-white italic">{{ $location->dimension }}</strong>
+								</p>
+						@else
 								<p class="text-center text-2xl">
 										push the button to travel between universes
 								</p>
-
+						@endif
 				</div>
 
 				<a href="{{ route('portal.show') }}" class="btn w-10/12 md:w-4/12" id="travel-btn">
