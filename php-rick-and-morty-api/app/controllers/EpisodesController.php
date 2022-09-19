e288cf28d3a1f2ab2985673e77d55d690b66a409
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-09-19 10:54:41 +0430

    implement fetch episodes data and display them in episodes page

--- php-rick-and-morty-api/app/controllers/EpisodesController.php
+++ php-rick-and-morty-api/app/controllers/EpisodesController.php
@@ -1,12 +1,21 @@
 <?php
 namespace app\controllers;
 
+use app\utilities\Config;
 use Buki\Router\Http\Controller;
+use app\core\adapter\GuzzleAdapter;
 
 class EpisodesController extends Controller
 {
 	public function index()
 	{
-		view('episodes');
+		$allEpisodes = GuzzleAdapter::response(Config::get('api.allEpisodes'), ['page' => 1]);
+		view('episodes', compact('allEpisodes'));
+	}
+
+	public function pagination(int $id)
+	{
+		$allEpisodes = GuzzleAdapter::response(Config::get('api.allEpisodes'), ['page' => $id]);
+		view('episodes', compact('allEpisodes'));
 	}
 }
--- php-rick-and-morty-api/resource/views/episodes.blade.php
+++ php-rick-and-morty-api/resource/views/episodes.blade.php
@@ -10,29 +10,39 @@
 				</div>
 
 				<div class="card-wrapper">
-						<div class="card gap-5">
-								<h3 class="card-title">#</h3>
-								<ul class="space-y-3 capitalize">
-										<li>
-												<span>episode number :</span>
-												<span>#</span>
-										</li>
-										<li>
-												<span>episode date :</span>
-												<span>#</span>
-										</li>
-										<li>
-												<span>count of characters :</span>
-												<span>#</span>
-										</li>
-								</ul>
-						</div>
+						@forelse ($allEpisodes->results as $episode)
+								<div class="card gap-5">
+										<h3 class="card-title truncate" title="{{ $episode->name ?? 'not defined' }}">
+												{{ $episode->name ?? 'not defined' }}
+										</h3>
+										<ul class="space-y-3 capitalize">
+												<li>
+														<span>episode number :</span>
+														<span>{{ $episode->episode ?? 'not defined' }}</span>
+												</li>
+												<li>
+														<span>episode date :</span>
+														<span>{{ $episode->air_date ?? 'not defined' }}</span>
+												</li>
+												<li>
+														<span>count of characters :</span>
+														<span>{{ count($episode->characters) ?? 'not defined' }}</span>
+												</li>
+										</ul>
+								</div>
+						@empty
+								<div class="col-span-12 rounded-lg bg-slate-800 p-7 text-center">
+										<p>sorry. there is no data to display</p>
+								</div>
+						@endforelse
 				</div>
 
 				<div class="flex justify-center">
 						<div class="flex items-center gap-2">
-								<a href="#top" class="btn cursor-pointer px-4 py-2">prev</a>
-								<a href="#top" class="btn cursor-pointer px-4 py-2">next</a>
+								<a href="{{ route('episodes.pagination', ['id' => page($allEpisodes->info->prev)]) }}"
+										class="btn cursor-pointer px-4 py-2">prev</a>
+								<a href="{{ route('episodes.pagination', ['id' => page($allEpisodes->info->next)]) }}"
+										class="btn cursor-pointer px-4 py-2">next</a>
 						</div>
 				</div>
 		</main>
